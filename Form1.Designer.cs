f056cd052c7b99d7d1388ea92e6ebb51ab86d1fa
Débora Vieira <deboraluizafono@gmail.com>
2024-10-14 10:24:22 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace _3935_DeboraVieira_T01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_hr_duracao = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbl_hr_chegada = new System.Windows.Forms.Label();
+            this.lbl_minuto_chegada = new System.Windows.Forms.Label();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.lbl_resultado = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_hr_partida = new System.Windows.Forms.TextBox();
+            this.txt_minuto_partida = new System.Windows.Forms.TextBox();
+            this.txt_hr_duracao = new System.Windows.Forms.TextBox();
+            this.txt_minuto_duracao = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(38, 216);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Hora";
+            // 
+            // lbl_hr_duracao
+            // 
+            this.lbl_hr_duracao.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_hr_duracao.Location = new System.Drawing.Point(29, 258);
+            this.lbl_hr_duracao.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_hr_duracao.Name = "lbl_hr_duracao";
+            this.lbl_hr_duracao.Size = new System.Drawing.Size(0, 0);
+            this.lbl_hr_duracao.TabIndex = 1;
+            this.lbl_hr_duracao.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(183, 216);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Minutos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 366);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Hora";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(184, 366);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 24);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Minutos";
+            // 
+            // lbl_hr_chegada
+            // 
+            this.lbl_hr_chegada.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_hr_chegada.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_hr_chegada.Location = new System.Drawing.Point(29, 405);
+            this.lbl_hr_chegada.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_hr_chegada.Name = "lbl_hr_chegada";
+            this.lbl_hr_chegada.Size = new System.Drawing.Size(76, 31);
+            this.lbl_hr_chegada.TabIndex = 3;
+            this.lbl_hr_chegada.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_minuto_chegada
+            // 
+            this.lbl_minuto_chegada.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_minuto_chegada.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_minuto_chegada.Location = new System.Drawing.Point(187, 405);
+            this.lbl_minuto_chegada.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_minuto_chegada.Name = "lbl_minuto_chegada";
+            this.lbl_minuto_chegada.Size = new System.Drawing.Size(75, 31);
+            this.lbl_minuto_chegada.TabIndex = 4;
+            this.lbl_minuto_chegada.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btn_calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_calcular.ForeColor = System.Drawing.Color.Black;
+            this.btn_calcular.Location = new System.Drawing.Point(298, 388);
+            this.btn_calcular.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(124, 38);
+            this.btn_calcular.TabIndex = 5;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = false;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btn_limpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_limpar.ForeColor = System.Drawing.Color.Black;
+            this.btn_limpar.Location = new System.Drawing.Point(298, 441);
+            this.btn_limpar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(124, 38);
+            this.btn_limpar.TabIndex = 6;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.UseVisualStyleBackColor = false;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
+            // 
+            // lbl_resultado
+            // 
+            this.lbl_resultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_resultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_resultado.Location = new System.Drawing.Point(13, 494);
+            this.lbl_resultado.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_resultado.Name = "lbl_resultado";
+            this.lbl_resultado.Size = new System.Drawing.Size(474, 51);
+            this.lbl_resultado.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(464, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Partida                                                                 ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(15, 171);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(472, 24);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Duração                                                                ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(38, 63);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 24);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Hora";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(184, 63);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 24);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Minutos";
+            // 
+            // txt_hr_partida
+            // 
+            this.txt_hr_partida.Location = new System.Drawing.Point(29, 102);
+            this.txt_hr_partida.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_hr_partida.Name = "txt_hr_partida";
+            this.txt_hr_partida.Size = new System.Drawing.Size(76, 20);
+            this.txt_hr_partida.TabIndex = 8;
+            this.txt_hr_partida.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_hr_partida.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_hr_partida_KeyPress);
+            this.txt_hr_partida.Validating += new System.ComponentModel.CancelEventHandler(this.txt_hr_partida_Validating_1);
+            // 
+            // txt_minuto_partida
+            // 
+            this.txt_minuto_partida.Location = new System.Drawing.Point(187, 102);
+            this.txt_minuto_partida.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_minuto_partida.Name = "txt_minuto_partida";
+            this.txt_minuto_partida.Size = new System.Drawing.Size(76, 20);
+            this.txt_minuto_partida.TabIndex = 9;
+            this.txt_minuto_partida.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_minuto_partida.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_minuto_partida_KeyPress);
+            this.txt_minuto_partida.Validating += new System.ComponentModel.CancelEventHandler(this.txt_minuto_partida_Validating);
+            // 
+            // txt_hr_duracao
+            // 
+            this.txt_hr_duracao.Location = new System.Drawing.Point(29, 258);
+            this.txt_hr_duracao.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_hr_duracao.Name = "txt_hr_duracao";
+            this.txt_hr_duracao.Size = new System.Drawing.Size(76, 20);
+            this.txt_hr_duracao.TabIndex = 10;
+            this.txt_hr_duracao.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_hr_duracao.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_hr_duracao_KeyPress);
+            this.txt_hr_duracao.Validating += new System.ComponentModel.CancelEventHandler(this.txt_hr_duracao_Validating);
+            // 
+            // txt_minuto_duracao
+            // 
+            this.txt_minuto_duracao.Location = new System.Drawing.Point(188, 258);
+            this.txt_minuto_duracao.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txt_minuto_duracao.Name = "txt_minuto_duracao";
+            this.txt_minuto_duracao.Size = new System.Drawing.Size(76, 20);
+            this.txt_minuto_duracao.TabIndex = 11;
+            this.txt_minuto_duracao.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_minuto_duracao.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_minuto_duracao_KeyPress);
+            this.txt_minuto_duracao.Validating += new System.ComponentModel.CancelEventHandler(this.txt_minuto_duracao_Validating);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(15, 311);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(478, 24);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Chegada                                                                ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 570);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_hr_chegada);
+            this.Controls.Add(this.lbl_minuto_chegada);
+            this.Controls.Add(this.btn_limpar);
+            this.Controls.Add(this.txt_minuto_duracao);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.txt_hr_duracao);
+            this.Controls.Add(this.txt_minuto_partida);
+            this.Controls.Add(this.txt_hr_partida);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_resultado);
+            this.Controls.Add(this.lbl_hr_duracao);
+            this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Companhia Só Ida.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_hr_duracao;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_minuto_chegada;
+        private System.Windows.Forms.Label lbl_hr_chegada;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_limpar;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Label lbl_resultado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_hr_partida;
+        private System.Windows.Forms.TextBox txt_minuto_partida;
+        private System.Windows.Forms.TextBox txt_hr_duracao;
+        private System.Windows.Forms.TextBox txt_minuto_duracao;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3935_DeboraVieira_T01
+{
+    public partial class Form1 : Form
+    {
+       
+
+            public Form1()
+        {
+            InitializeComponent();
+        }
+ 
+        private void txt_hr_partida_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if(!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void txt_minuto_partida_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txt_hr_duracao_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txt_minuto_duracao_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txt_hr_partida_Validating_1(object sender, CancelEventArgs e)
+        {
+            int hora_partida = int.Parse(txt_hr_partida.Text);
+            if(hora_partida < 0 || hora_partida > 23){
+                    MessageBox.Show("A hora informada deve ser entre 0 e 23","Erro!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txt_hr_duracao_Validating(object sender, CancelEventArgs e)
+        {
+            int hora_duracao = int.Parse(txt_hr_duracao.Text);
+            if (hora_duracao < 0 || hora_duracao > 23)
+            {
+                MessageBox.Show("A hora informada deve ser entre 0 e 23.", "Erro!",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txt_minuto_partida_Validating(object sender, CancelEventArgs e)
+        {
+            int minuto_partida = int.Parse(txt_minuto_partida.Text);
+            if( minuto_partida <0 || minuto_partida > 59)
+            {
+                MessageBox.Show("O minuto informado deve ser entre 0 e 59.", "Erro!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void txt_minuto_duracao_Validating(object sender, CancelEventArgs e)
+        {
+            int minuto_duracao = int.Parse(txt_minuto_duracao.Text);
+            if (minuto_duracao < 0 || minuto_duracao > 59)
+            {
+                MessageBox.Show("O minuto informado deve ser entre 0 e 59.", "Erro!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            int hora_partida;
+            int minutos_partida;
+            int hora_duracao;
+            int minuto_duracao;
+
+            try { 
+                 hora_partida = int.Parse(txt_hr_partida.Text);
+                 minutos_partida = int.Parse(txt_minuto_partida.Text);
+                 hora_duracao = int.Parse(txt_hr_duracao.Text);
+                 minuto_duracao = int.Parse(txt_minuto_duracao.Text);
+            }
+            catch
+            {
+                 MessageBox.Show("Todos campos devem ser preenchidos!", "Erro!",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int hora_chegada = hora_partida + hora_duracao;
+            if (hora_chegada > 23)
+            {
+                hora_chegada = hora_chegada % 24;
+            }
+
+            int minutos_chegada = minutos_partida + minuto_duracao;
+            if (minutos_chegada > 59)
+            {
+                int hora_adicional = minutos_chegada / 60;
+                int resto_minutos = minutos_chegada % 60;
+                hora_chegada += hora_adicional;
+                minutos_chegada = resto_minutos;
+
+            }
+            string DiaChegada = hora_partida + hora_duracao > 23 ? "dia seguinte" : "mesmo dia";
+            lbl_hr_chegada.Text = Convert.ToString(hora_chegada);
+            lbl_minuto_chegada.Text = Convert.ToString(minutos_chegada);
+            lbl_resultado.Text = "O seu voo chegará às " + lbl_hr_chegada.Text
+                        + " horas e " + lbl_minuto_chegada.Text + " minutos do " + DiaChegada;
+        }
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            txt_hr_partida.ResetText();
+            txt_minuto_partida.ResetText();
+            txt_hr_duracao.ResetText();
+            txt_minuto_duracao.ResetText();
+            lbl_hr_chegada.ResetText();
+            lbl_minuto_chegada.ResetText();
+            lbl_resultado.ResetText();
+        }
+    }
+}
